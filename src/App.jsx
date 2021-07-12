78c9ef09470f3cb6de09ebb03d8d12e165e9c3b9
Rauf <nraufu@gmail.com>
2021-07-12 21:26:04 +0200

    router fix :adhesive_bandage:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
 
-const App = () => <Header />;
+const App = () => {
+    return (
+        <Fragment>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={Home} />
+            </Switch>
+        </Fragment>
+    );
+};
 
 export default App;
