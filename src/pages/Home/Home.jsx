a271fd4ea11b25cbc7938afdd5bd06c947c2df75
niyonzi rauf <nraufu@gmail.com>
2021-08-01 22:23:59 +0200

    image gallery

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import hero from '../../assets/images/hero.png';
 import Footer from '../../parts/Footer/Footer';
+import ImageLibrary from './../../parts/Gallery/Gallery';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
                 className='hero'
                 style={{ backgroundImage: `url(${hero})` }}
             ></div>
+            <ImageLibrary />
             <Footer />
         </Fragment>
     );
