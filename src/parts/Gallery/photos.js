cd634b4aac048ea635b84c78d05c8cf5ce12f311
niyonzi rauf <nraufu@gmail.com>
2021-09-03 08:11:03 +0200

    improvements :sparkles:

--- src/parts/Gallery/photos.js
+++ src/parts/Gallery/photos.js
@@ -1,18 +1,15 @@
-import image5 from '../../assets/images/image5.jpg';
-// import image1 from '../../assets/images/image1.jpg';
+import image1 from '../../assets/images/image1.jpg';
 import image2 from '../../assets/images/image2.jpg';
 import image3 from '../../assets/images/image3.jpg';
 import image4 from '../../assets/images/image4.jpg';
 import image10 from '../../assets/images/image10.jpg';
 import image11 from '../../assets/images/image11.jpg';
+import image12 from '../../assets/images/image12.jpg';
+import image13 from '../../assets/images/image13.jpg';
+import image14 from '../../assets/images/image14.jpg';
 
 
 export const photos = [
-  {
-    src: image5,
-    width: 3,
-    height: 4
-  },
   {
     src: image3,
     width: 4,
@@ -23,15 +20,15 @@ export const photos = [
     width: 3,
     height: 4
   },
-  // {
-  //   src: image1,
-  //   width: 3,
-  //   height: 4
-  // },
   {
     src: image4,
     width: 3,
     height: 4
+  },
+  {
+    src: image14,
+    width: 4,
+    height: 3
   },
   {
     src: image10,
@@ -44,13 +41,18 @@ export const photos = [
     height: 4
   },
   {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    src: image1,
+    width: 3,
+    height: 3
+  },
+  {
+    src: image12,
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    src: image13,
     width: 4,
     height: 3
-  }
+  },
 ];
